7adc45f30ca1670a7c50b93bedd6de4a4c428ab7
Jay Mundrawala <jdmundrawala@gmail.com>
2015-03-29 16:50:04 -0700

    Remove monkey patch

--- lib/kitchen-appbundle-updater.rb
+++
@@ -1,1 +0,0 @@
-require 'kitchen-appbundle-updater/monkey'
--- lib/kitchen-appbundle-updater/monkey.rb
+++
@@ -1,9 +0,0 @@
-require 'kitchen/provisioner/chef_base'
-require 'kitchen-appbundle-updater/helpers'
-
-class Kitchen::Provisioner::ChefBase
-
-  default_config :chef_gitref, "master"
-
-
-end
