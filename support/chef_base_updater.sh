65fc0fbbd6ab1268fa6a7f600629f0fa4a6bf5de
Jay Mundrawala <jdmundrawala@gmail.com>
2015-04-21 10:25:18 -0700

    Use chef-zero instead of chef-solo

--- support/chef_base_updater.sh
+++ support/chef_base_updater.sh
@@ -4,7 +4,7 @@ set -e
 main() {
   url=`curl -s "https://api.github.com/repos/jdmundrawala/chef-appbundle-updater/releases/latest" | \
     /opt/chef/embedded/bin/ruby -rjson -e 'j=JSON.parse(STDIN.read); puts j["assets"][0]["browser_download_url"]'`
-  sudo /opt/chef/bin/chef-solo -r "$url" -o "recipe[chef-appbundle-updater::default]"
+  sudo /opt/chef/bin/chef-client -z --recipe-url "$url" -o "recipe[chef-appbundle-updater::default]"
 }
 
 # augment path in an attempt to find a download program
