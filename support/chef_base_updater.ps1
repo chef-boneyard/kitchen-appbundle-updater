2f97a8dfcf90fb6b1ebb0dbaf282292ac918eb59
Jay Mundrawala <jdmundrawala@gmail.com>
2015-04-21 21:28:40 -0700

    Fix untar for windows

--- support/chef_base_updater.ps1
+++ support/chef_base_updater.ps1
@@ -4,6 +4,8 @@ Function Log($m) { Write-Host "       $m`n" }
 
 Function Update-Chef($gitorg, $gitrepo, $gitref, $dstdir) {
   Log "Finding cookbook to update chef"
+  Log $env:Path
+  $env:Path = $env:Path + ";$chef_omnibus_root\bin\"
   $response = Invoke-WebRequest "https://api.github.com/repos/jdmundrawala/chef-appbundle-updater/releases/latest"
   $url = ($response.Content | ConvertFrom-Json).assets[0].browser_download_url
   Log "Running chef-client with recipe chef-appbundle-updater::default from $url"
