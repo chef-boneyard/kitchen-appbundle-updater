718508b61cae3a699928abca1c6c05822a4a1fc0
Lamont Granquist <lamont@scriptkiddie.org>
2015-09-24 22:51:40 -0700

    working POC of using the appbundle-update gem

--- lib/kitchen/provisioner/chef_github.rb
+++ lib/kitchen/provisioner/chef_github.rb
@@ -1,5 +1,4 @@
 require 'kitchen/provisioner/chef_zero'
-require 'kitchen-appbundle-updater/helpers'
 
 module Kitchen
   module Provisioner
@@ -11,24 +10,30 @@ module Kitchen
       def prepare_command
         gem_bin = remote_path_join(config[:ruby_bindir], "gem").
           tap { |path| path.concat(".bat") if windows_os? }
+        appbundle_updater_bin = remote_path_join(config[:ruby_bindir], "appbundle-updater").
+          tap { |path| path.concat(".bat") if windows_os? }
         vars = [
-          chef_client_zero_env,
           shell_var("refname", config[:refname]),
           shell_var("github_owner", config[:github_owner]),
           shell_var("github_repo", config[:github_repo]),
           shell_var("gem", sudo(gem_bin)),
+          shell_var("appbundle_updater", sudo(appbundle_updater_bin)),
         ].join("\n").concat("\n")
 
-        shell_code_from_file(vars, updater_script)
+        my_shell_code_from_file(vars, "chef_base_updater")
       end
 
       private
 
-      def updater_script(file, is_powershell)
-        File.join(
+      # need to override and use __FILE__ relative to this plugin
+      def my_shell_code_from_file(vars, file)
+        src_file = File.join(
           File.dirname(__FILE__),
-          %w[.. .. support],
-          "chef_base_updater" + (powershell_shell? ? ".ps1" : ".sh")
+          %w[.. .. .. support],
+          file + (powershell_shell? ? ".ps1" : ".sh")
+        )
+
+        wrap_shell_code([vars, "", IO.read(src_file)].join("\n"))
       end
 
     end
