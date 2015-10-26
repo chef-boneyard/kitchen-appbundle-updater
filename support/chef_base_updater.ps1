c17b6cb2dc31daa3695e468c33d4f52e2bc62b3a
Lamont Granquist <lamont@scriptkiddie.org>
2015-10-26 13:06:07 -0700

    add some debugging to powershell

--- support/chef_base_updater.ps1
+++ support/chef_base_updater.ps1
@@ -1,4 +1,6 @@
 $ErrorActionPreference = "Stop";
 
+Write-Host "-----> Installing appbundle-updater gem`n"
 & "$gem" install appbundle-updater
+Write-Host "-----> Running appbundle-updater to upgrade chef inside omnibus-chef`n"
 & "$appbundle_updater" chef chef "$refname" --tarball --github "$github_owner/$github_repo"
