b6d2dff9ffb10820de11371fdbae05b506e16f31
Lamont Granquist <lamont@scriptkiddie.org>
2015-10-26 18:25:16 -0700

    more tweaks to windows

--- lib/kitchen/provisioner/chef_github.rb
+++ lib/kitchen/provisioner/chef_github.rb
@@ -8,16 +8,19 @@ module Kitchen
       default_config :github_repo, "chef"
 
       def prepare_command
+        ruby_bin = remote_path_join(config[:ruby_bindir], "ruby").
+          tap { |path| path.concat(".exe") if windows_os? }
         gem_bin = remote_path_join(config[:ruby_bindir], "gem").
           tap { |path| path.concat(".bat") if windows_os? }
-        appbundle_updater_bin = remote_path_join(config[:ruby_bindir], "appbundle-updater").
-          tap { |path| path.concat(".bat") if windows_os? }
+        appbundle_updater_bin = remote_path_join(config[:ruby_bindir], "appbundle-updater")
+          # tap { |path| path.concat(".bat") if windows_os? }
         vars = [
           shell_var("refname", config[:refname]),
           shell_var("github_owner", config[:github_owner]),
           shell_var("github_repo", config[:github_repo]),
+          shell_var("ruby", sudo(ruby_bin)),
           shell_var("gem", sudo(gem_bin)),
-          shell_var("appbundle_updater", sudo(appbundle_updater_bin)),
+          shell_var("appbundle_updater", appbundle_updater_bin),
         ].join("\n").concat("\n")
 
         my_shell_code_from_file(vars, "chef_base_updater")
--- support/chef_base_updater.ps1
+++ support/chef_base_updater.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = "Stop";
 
 Write-Host "-----> Installing appbundle-updater gem`n"
 & "$gem" install appbundle-updater
-Write-Host "-----> Running appbundle-updater to upgrade chef inside omnibus-chef`n"
-& "$appbundle_updater" chef chef "$refname" --tarball --github "$github_owner/$github_repo"
+Write-Host "-----> Running appbundle-updater to upgrade chef inside omnibus-chef (takes awhile)`n"
+& "$ruby" "$appbundle_updater" chef chef "$refname" --tarball --github "$github_owner/$github_repo"
