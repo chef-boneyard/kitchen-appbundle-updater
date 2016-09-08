baec7718b9c679d401e8f3ee3466d864974568d3
John Keiser <john@johnkeiser.com>
2016-09-08 10:00:55 -0700

    Use a branch to install appbundle-updater

--- support/chef_base_updater.sh
+++ support/chef_base_updater.sh
@@ -1,4 +1,14 @@
-set -e
+set -evx
 
-$gem install appbundler appbundle-updater
+export PATH=/opt/chef/bin:/opt/chef/embedded/bin:$PATH
+sudo yum -y install git
+pwd
+git clone -v https://github.com/chef/appbundle-updater /appbundle-updater
+pushd /appbundle-updater
+pwd
+git checkout jk/bundle-without-groups
+bundle install
+bundle exec rake install
+popd
+$gem install appbundler
 $ruby $appbundle_updater chef chef $refname --tarball --github $github_owner/$github_repo
