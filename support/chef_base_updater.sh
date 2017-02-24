1f76a042f92a18621d4de9055aaf54c2acd11fa8
Lamont Granquist <lamont@scriptkiddie.org>
2017-02-24 14:09:23 -0800

    add ohai_refname so we can pull ohai from github
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- support/chef_base_updater.sh
+++ support/chef_base_updater.sh
@@ -1,4 +1,5 @@
 set -e
 
 $gem install appbundler appbundle-updater
+[ ! -z "$ohai_refname" ] && $ruby $appbundle_updater chef ohai $ohai_refname --tarball --github chef/ohai
 $ruby $appbundle_updater chef chef $refname --tarball --github $github_owner/$github_repo
